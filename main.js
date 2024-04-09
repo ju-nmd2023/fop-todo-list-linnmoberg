2440a7c5c5b8ba39cf0220324c2bbe747106e960
linnmoberg <linn1.moberg@gmail.com>
2024-04-09 16:31:01 +0200

    javascript

--- main.js
+++ main.js
@@ -1,11 +1,58 @@
 //loading content from the html
+
+let tasks = [];
+const inputElement = document.querySelector(".box");
+const addUlItem = document.getElementById("list-container");
+
 window.addEventListener("load", () => {
 
     const addButtonElement = document.querySelector(".button");
-    addButtonElement.addEventListener("click", clickAddButton);
+    addButtonElement.addEventListener("click", addTask);
 });
 
-function clickAddButton () {
-    console.log("Clicked");
+function addTask() {
+    let inputFieldText = inputElement.value;
+    let task = {taskName: inputFieldText};
+    tasks.push(task);
+    showTasks();
+    inputElement.value=""
 }
+
+function showTasks() {
+    addUlItem.innerHTML = "";
+    for (i=0; i < tasks.length; i++) {
+    let liTaskElement = document.createElement("li");
+    
+    liTaskElement.innerText = tasks[i].taskName;
+
+      addUlItem.appendChild(liTaskElement);
+
+      tasks.addEventListener = 
+    }
+}
+
+// function clickAddButton () {
+    
+//     console.log("clicked");
+    
+//     // addButtonElement.addEventListener("click", function () {
+//         const inputValue = inputElement.value;
+//         if (inputValue !== "") {
+//             createTask(inputValue);
+//             inputElement.value = "";
+            
+
+//             // createTask(inputValue);
+
+//             // inputElement.value = "";
+//         }
+//         else {
+//             window.alert("Enter Text");
+//         }
+//     }
+// // }
+
+// function createTask(taskText) {
+// console.log("Creating task", taskText);
+// }
 
