cea3fb5aa933a1f5f512f2e38f8a35f5ff46d09e
linnmoberg <linn1.moberg@gmail.com>
2024-04-12 16:15:17 +0200

    NEW JAVASCRIPT

--- main.js
+++ main.js
@@ -5,62 +5,53 @@ const inputElement = document.querySelector(".box");
 const addUlItem = document.getElementById("list-container");
 
 window.addEventListener("load", () => {
-
-    const addButtonElement = document.querySelector(".button");
-    addButtonElement.addEventListener("click", addTask);
+  const addButtonElement = document.querySelector(".button");
+  addButtonElement.addEventListener("click", addTask);
 });
 
 function addTask() {
-    let inputFieldText = inputElement.value;
-    let task = { taskName: inputFieldText, done: "img/check-mark.png", remove: "img/x-mark.png"};
-    tasks.push(task);
-    showTasks();
-    inputElement.value=""
+  let inputFieldText = inputElement.value;
+  let task = {
+    taskName: inputFieldText,
+    done: "img/check-mark.png",
+    remove: "img/x-mark.png",
+  };
+  tasks.push(task);
+  showTasks();
+  inputElement.value = "";
 }
 
 function showTasks() {
-    addUlItem.innerHTML = "";
-    for (i=0; i < tasks.length; i++) {
+  addUlItem.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
     let liTaskElement = document.createElement("li");
-    
+
     liTaskElement.innerText = tasks[i].taskName;
 
-      addUlItem.appendChild(liTaskElement);
+    addUlItem.appendChild(liTaskElement);
 
-      const imgElement = document.createElement("img");
-      imgElement.classList.add(".imagechecked");
-      imgElement.src = tasks[i].done;
-      addUlItem.appendChild(imgElement);
-      
-      const imgElement2 = document.createElement("img");
-      imgElement2.classList.add(".imageremove");
-      imgElement2.src = tasks[i].remove;
-      addUlItem.appendChild(imgElement2);
-    }
+    const imgElementDone = document.createElement("img");
+    imgElementDone.classList.add(".imagechecked");
+    imgElementDone.src = tasks[i].done;
+    imgElementDone.width = 40;
+    addUlItem.appendChild(imgElementDone);
+
+    imgElementDone.addEventListener("click", function () {
+      liTaskElement.style.textDecoration = "line-through";
+    });
+
+    const imgElementRemove = document.createElement("img");
+    imgElementRemove.classList.add(".imageremove");
+    imgElementRemove.src = tasks[i].remove;
+    imgElementRemove.width = 40;
+    addUlItem.appendChild(imgElementRemove);
+    let index = i
+
+    imgElementRemove.addEventListener("click", function () {
+      tasks.splice(index, 1);
+      showTasks();
+    });
+
+  }
 }
-
-// function clickAddButton () {
-    
-//     console.log("clicked");
-    
-//     // addButtonElement.addEventListener("click", function () {
-//         const inputValue = inputElement.value;
-//         if (inputValue !== "") {
-//             createTask(inputValue);
-//             inputElement.value = "";
-            
-
-//             // createTask(inputValue);
-
-//             // inputElement.value = "";
-//         }
-//         else {
-//             window.alert("Enter Text");
-//         }
-//     }
-// // }
-
-// function createTask(taskText) {
-// console.log("Creating task", taskText);
-// }
 
