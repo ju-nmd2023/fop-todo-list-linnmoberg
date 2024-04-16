0573c4fe059102b985726f4e9cac6891aa089894
linnmoberg <linn1.moberg@gmail.com>
2024-04-16 16:51:57 +0200

    beggining of storage

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 //loading content from the html
 
+// () => {
+//     if (localStorage.getItem("tasks")) {
+//         tasks = JSON.parse(localStorage.getItem("tasks"));
+//         showTasks();
+//     }
+// })
+
+// function saveData() {
+//     localStorage.setItem("tasks", listcontainer.innerHTML);
+// }
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 let tasks = [];
 const inputElement = document.querySelector(".box");
-const addUlItem = document.getElementById("list-container");
+const addUlItem = document.getElementById("listcontainer");
 
 window.addEventListener("load", () => {
   const addButtonElement = document.querySelector(".button");
   addButtonElement.addEventListener("click", addTask);
 });
 
+// The next lines I got help from one of the help assistents on the labs
 function addTask() {
   let inputFieldText = inputElement.value;
   let task = {
@@ -40,18 +56,27 @@ function showTasks() {
       liTaskElement.style.textDecoration = "line-through";
     });
 
+    // The next 3 lines I got help from one of the lab assistens 
     const imgElementRemove = document.createElement("img");
     imgElementRemove.classList.add(".imageremove");
     imgElementRemove.src = tasks[i].remove;
     imgElementRemove.width = 40;
     addUlItem.appendChild(imgElementRemove);
+    // The next line I got help from Garrit under one of the labs
     let index = i
 
     imgElementRemove.addEventListener("click", function () {
       tasks.splice(index, 1);
+      saveTasks();
       showTasks();
     });
 
   }
 }
+
+window.addEventListener("load", showTasks);
+
+localStorage.color = tasks;
+
+
 
